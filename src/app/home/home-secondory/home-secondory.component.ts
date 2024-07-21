de9bbe0c9e5e68f1b08a88595a53940a2e532736
Enrique <enriquegrassporras@gmail.com>
2024-07-20 23:09:16 -0400

    ok

--- src/app/home/home-secondory/home-secondory.component.ts
+++ src/app/home/home-secondory/home-secondory.component.ts
@@ -1,6 +1,6 @@
 import { NgClass } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {  FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Category, DatosCategories } from '../../models/localDatos.moduls';
 import { StoresComponent } from "./stores/stores.component";
 
@@ -13,15 +13,44 @@ import { StoresComponent } from "./stores/stores.component";
 })
 export class HomeSecondoryComponent implements OnInit{
 
-  tolbar =[{item:'Distance',ico:'none'},{item:'Ratings',ico:'none'}]
+  @Output() rangeChangeEvent = new EventEmitter<number>();
+
   options = ['$0', '$288', '$576', '$864'];
   selectedValue = 0;
   selectedOptionIndex = 0;
   category:Category[] = []
   isOpen = false;
+  miCheckbox = false;
+  toolbar:any[] = [
+    { item: 'Distance', check: false, ico: 'none' },
+    { item: 'Ratings', check: false, ico: 'none' }
+  ];
 
-  toggle() {
-    this.isOpen = !this.isOpen;
+  miFormulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.miFormulario = this.fb.group({
+      checkbox1:[false],
+      checkbox2:[false]
+    });
+  /*   this.toolbar.forEach(item => {
+      const control = new FormControl(item.check);
+      this.miFormulario.setControl(item.item, control);
+    }); */
+  }
+
+  /* profileForn = new FormGroup({
+    checkbox: new FormControl(false),
+    checkbox2: new FormControl(false),
+  }) */
+
+  toggleCheck(item: string): void {
+   /*  this.miFormulario.value.setValue(!this.miFormulario.value); */
+   console.log(this.miFormulario.value)
+  }
+
+  toggleCheckCategory(item: string): void {
+    this.miFormulario.controls[item].setValue(!this.miFormulario.controls[item].value);
   }
 
   onRangeChange(event: any): void {
@@ -29,6 +58,10 @@ export class HomeSecondoryComponent implements OnInit{
     this.selectedValue = Math.round(value / 325); // Ajusta este valor según tus necesidades
     this.selectOption(this.selectedValue);
   }
+  manejarEventoDelHijo(data: {value: number, message: string}): void {
+    console.log(data.value, data.message);
+  }
+
 
   selectOption(index: number): void {
     this.selectedOptionIndex = index;
@@ -38,4 +71,6 @@ export class HomeSecondoryComponent implements OnInit{
   ngOnInit(): void {
    this.category = DatosCategories
   }
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       backgroundImage: {
         "mi-image": "url('')",
       },
+      boxShadow: {
+        custom:
+          "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      },
     },
   },
   plugins: [],
